402b0e95b516e313706e6dcf66cd89ea59a1a926
Darwin Ameli <DIDI.ameli@gmail.com>
2018-11-09 15:33:02 +0100

    Improve path finding
    Now the solver will search for the best solution of the maze

--- maze.java
+++ maze.java
@@ -1,7 +1,10 @@
 //Thanks shlomibabluki
-public class Maze {
+public class Maze_Best {
 
-  public int counter = 0;
+public int counter = 0;
+private final static int MAX_VALUE = 1000;	
+	
+	int best_solution = MAX_VALUE;
 	
 	public char[][] maze =
 		   {{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
@@ -14,61 +17,84 @@ public class Maze {
 			{'#', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', '#'},
 			{'#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
 			{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},};
+	
+	public char[][] output_maze = null;
+	
+	private void cloneMaze() {
+		output_maze = new char[10][10];
+		for (int x=0; x<10; x++) {
+			for (int y=0; y<10; y++) {
+				output_maze[x][y] = maze[x][y];
+			}
+		}
+	}
 
 	// Get the start location (x,y) and try to solve the maze
 	public void solve(int x, int y) {
-		if (step(x,y)) {
-			maze[x][y] = 'S';
+		best_solution = MAX_VALUE;
+		
+		if (step(x,y, 0) != MAX_VALUE) {
+			output_maze[x][y] = 'S';
 		}
 	}
 	
 	// Backtracking method
-	public boolean step (int x, int y) {
+	public int step (int x, int y, int count) {
 		
 		counter++;
-		
 		//System.out.println(this.toString());
 		
 		/** Accept case - we found the exit **/
 		if (maze[x][y] == 'X') {
-			return true;
+			best_solution = count;
+			this.cloneMaze();
+			return count;
 		}
 		
-		/** Reject case - we hit a wall or our path **/
+		/** Reject case - we are hit a wall or our path **/
 		if (maze[x][y] == '#' || maze[x][y] == '*') {
-			return false;
+			return MAX_VALUE;
+		}
+		/** Reject case - we already have a better solution! **/
+		if (count == best_solution) {
+			return MAX_VALUE;
 		}
 		
 		/** Backtracking Step **/
 		
-		// Mark this location as part of our path
+		// Mark this location as part of out path
 		maze[x][y] = '*';
-		boolean result;	
+		int result = MAX_VALUE;	
+		
+		int new_result = MAX_VALUE;
 		
 		// Try to go Right
-		result = step(x, y+1);
-		if (result) { return true;}
+		new_result = step(x, y+1, count+1);
+		if (new_result < result) { result = new_result;}
 		
 		// Try to go Up
-		result = step(x-1, y);
-		if (result) { return true;}
+		new_result = step(x-1, y, count+1);
+		if (new_result < result) { result = new_result;}
 		
 		// Try to go Left
-		result = step(x, y-1);
-		if (result) { return true;}		
+		new_result = step(x, y-1, count+1);
+		if (new_result < result) { result = new_result;}	
 		
 		// Try to go Down
-		result = step(x+1, y);
-		if (result) { return true;}		
-		
-		
-		/** Deadend - this location can't be part of the solution **/
+		new_result = step(x+1, y, count+1);
+		if (new_result < result) { result = new_result;}	
 		
 		// Unmark this location
 		maze[x][y] = ' ';
 		
+		if (result < MAX_VALUE) {
+			return result;
+		}
+		
+		/** Deadend - this location can't be part of the solution **/
+		
 		// Go back
-		return false;
+		return MAX_VALUE;
 	}
 	
 	public String toString() {
@@ -82,14 +108,28 @@ public class Maze {
 		return output;
 	}
 	
+	public String toStringSolution() {
+		if (output_maze == null) {
+			return "No Solution!";
+		}		
+		String output = "";
+		for (int x=0; x<10; x++) {
+			for (int y=0; y<10; y++) {
+				output += output_maze[x][y] + " ";
+			}
+			output += "\n";
+		}
+		return output;
+	}	
+	
 	public static void main(String[] args) {
-		Maze m = new Maze();
+		Maze_Best m = new Maze_Best();
 		// Locate the exit
 		m.maze[1][1] = 'X';
 		
 		// Start solving the maze from (8, 1)
 		m.solve(8, 1);
-		System.out.println(m);
+		System.out.println(m.toStringSolution());
 		System.out.println("Total calls: " + m.counter);
 	}
 
